658b65df3511479a01cbe07844f52475eed26a06
Jeff Benton <sikletz@gmail.com>
2019-04-23 01:55:40 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,7 +7,10 @@ def class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.string :location
       t.string :theme
       t.float :price
-      t.boolean :is_friendly
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.
     end
   end
 end
